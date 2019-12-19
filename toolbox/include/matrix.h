62f0c61b37a1f69b6d0e23e5e7d2d3b694be1a09
Boris Ulyanov <boris-ulyanov@mail.ru>
2019-12-19 22:46:19 +0300

    skeleton

--- toolbox/include/matrix.h
+++ toolbox/include/matrix.h
@@ -6,12 +6,49 @@
 
 #include <map>
 
-template <typename T, T default_value, uint8_t dimensions = 2>
+using index = uint32_t;
+
+template <typename T, T default_value, uint8_t deep_level>
+class DataSlice {
+    using mapped_type = DataSlice<T, default_value, deep_level - 1>;
+    using data_type = std::map<index, mapped_type>;
+    data_type data;
+
+   public:
+    std::size_t size() const {
+        std::size_t s = 0;
+        for (const auto a : data) s += a.second.size();
+        return s;
+    }
+};
+
+template <typename T, T default_value>
+class DataSlice<T, default_value, 0> {
+    using data_type = std::map<index, T>;
+    data_type data;
+
+   public:
+    std::size_t size() const {
+        return data.size();
+    }
+};
+
+// ==================
+
+template <typename T, T default_value, uint8_t dimension = 2>
 class Matrix {
-    int a;
+    static_assert(dimension > 0);
 
-public:
-    std::size_t size() {return 0;}
+    using data_type = DataSlice<T, default_value, dimension - 1>;
 
-    int operator()(uint32_t i, uint32_t j) {return 0;}
+    data_type data;
+
+   public:
+    std::size_t size() {
+        return data.size();
+    }
+
+    int operator()(index i, index j) {
+        return 0;
+    }
 };
